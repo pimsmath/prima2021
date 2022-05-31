02185d14db2a7ad8d0cec3764737c459789d0bb9
Ian Allison <iana@pims.math.ca>
2022-05-31 12:06:22 -0700

    Allow default speaker group to be selected via anchor
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- static/js/local.js
+++ static/js/local.js
@@ -1,12 +1,19 @@
-$(document).click(function(event) {
+console.log("Selecting Speaker group");
+
+var SpeakerGroupSelect = function() {
+  console.log("Speaker group " + hash);
   var hash = window.location.hash;
   if(hash.startsWith("#speakers")){
-      console.log("Speaker group " + hash);
       var speakerType = hash.split('-').pop();
-      $("#speaker-tab-buttons a.nav-link").removeClass("active");
-      $("#speaker-tab-buttons a.nav-link#" + speakerType + "-tab").addClass("active");
+      $("ul#speaker-tab-pills > li > a.nav-link").removeClass("active");
+      $("ul#speaker-tab-pills > li > a.nav-link" + "#" + speakerType + "-tab-pill").addClass("active");
   
-      $("#speakers div.tab-pane").removeClass("active in");
-      $("#speakers div.tab-pane#" + speakerType).addClass("active in");
+      $("#speaker-tabs div.tab-pane").removeClass("active in");
+      $("#speaker-tabs div.tab-pane" + "#" + speakerType + "-tab").addClass("active in");
   }
-})
+}
+
+window.onhashchange = SpeakerGroupSelect;
+$(document).ready(function() {
+    SpeakerGroupSelect();
+});
